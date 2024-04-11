ce8979abb60bfc4324824a25d1cf9251aaf448e9
AgentBlowfish <a01705196@tec.mx>
2024-04-10 18:48:03 -0600

    Filtros nuevos

--- practica_facturacion.py
+++ practica_facturacion.py
@@ -15,25 +15,27 @@ filtro1.to_csv("practica_facturacion-1.csv")
 
 filtro2 = df[~(df["CVE_VEND"] == 5.0 ) & ~(df["CVE_VEND"] == 4.0)]
 print(filtro2)
+filtro2.to_csv("practica_facturacion-2.csv")
 
 filtro3=df[df["FECHA_ENT"] == '2022-28-2']
 print(filtro3)
+filtro3.to_csv("practica_facturacion-3.csv")
 
 filtro4=df[(df["CAN_TOT"] < 5951.7)| (df["STATUS"] == "E")]
 print(filtro4)
+filtro4.to_csv("practica_facturacion-4.csv")
 
 filtro5 = df.iloc[ : , [0, 6, 7,9]]  
 print(filtro5)
+filtro5.to_csv("practica_facturacion-5.csv")
 
 filtro6 = df.iloc[7001:7099, :  ] 
 print(filtro6)
+filtro6.to_csv("practica_facturacion-6.csv")
 
 df1= pd.read_excel("datos_facturacion.xlsx", index_col=3)
 df1.head()
 
 filtro7=df1.loc[[1.0,2.0], ["FECHAELAB"]]
 print(filtro7)
-
-
-
-     
+filtro7.to_csv("practica_facturacion-7.csv")
